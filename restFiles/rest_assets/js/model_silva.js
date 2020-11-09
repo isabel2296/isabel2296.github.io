ece4d81be9f882e2ab1e89054424e80a3cbad248
Isabel Silva <70250018+isabel2296@users.noreply.github.com>
2020-11-08 22:30:50 -0800

    updated for new add ons and better specs
    updated

---
+++ restFiles/rest_assets/js/model_silva.js
@@ -0,0 +1,14 @@
+// this will contain the menu items 
+class Item{
+    constructor(unique_id, name, price,quantity){
+        this.unique_id = unique_id; 
+        this.name = name;
+        this.price = price; 
+        this.quantity = quantity; 
+        this.feature = false; 
+    }
+}
+
+class Menu{
+    constructor(){}
+}
