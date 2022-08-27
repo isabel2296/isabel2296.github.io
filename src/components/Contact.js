72e11efc2c45dd9f8460a79c8fb2c705f24a7f5d
Isabel Silva <70250018+isabel2296@users.noreply.github.com>
2022-08-26 17:31:08 -0700

    contact info namr

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,10 +10,16 @@ export default function Contact() {
                     <a />
                 </div>
                 <form className="contact-container-form">
+                    <div className='contact-name'>
                     <label className='contact-form-attribute'>
-                        Your Name:
-                        <input className='input-text' type="text" name="name"/>
+                        First Name:
+                        <input className='input-text' type="text" name="firstName"/>
                     </label>
+                    <label className='contact-form-attribute'>
+                        Last Name:
+                        <input className='input-text' type="text" name="lastName"/>
+                    </label>
+                    </div>
                     <label className='contact-form-attribute'>
                         Your Email:
                         <input className='input-text' type="text" name="email"/>
@@ -22,7 +28,7 @@ export default function Contact() {
                         Your Message:
                         <textarea  className='message-box' name="message"/>
                     </label>
-                    <input type="submit" value="Submit"/>
+                    <input className='contact-submit' type="submit" value="Submit"/>
                 </form>
            
             </div>
