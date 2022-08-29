6702ece35d960a3cf5e317a8baf47901c1132b6a
Isabel Silva <70250018+isabel2296@users.noreply.github.com>
2022-08-28 19:34:44 -0700

    contact netifly, change to emailjs.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,28 @@
 import '../styles/contact.css';
-
+import { useState } from 'react';
 export default function Contact() {
+    const [firstName, setFName] = useState("");
+    const [lastName, setLName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    function encode(data) {
+        return Object.keys(data)
+          .map(
+            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+          )
+          .join("&");
+      }
+    
+      function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", firstName, lastName, email, message }),
+        })
+          .then(() => alert("Message sent!"))
+          .catch((error) => alert(error));
+      }
     return(
         <section className="contact-section">
             <h2>Contact Me</h2>
@@ -9,26 +31,31 @@ export default function Contact() {
                 <div className='contact-me-personal-info'>
                     <a />
                 </div>
-                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST" className="contact-container-form">
+                <form netlify name="contact" onSubmit={handleSubmit}className="contact-container-form">
                     <div className='contact-name'>
                     <label className='contact-form-attribute'>
                         First Name:
-                        <input className='input-text' type="text" name="firstName"/>
+                        <input id="firstName" name="firstName"className='input-text' type="text" 
+                        onChange={(e) => setFName(e.target.value)}/>
                     </label>
                     <label className='contact-form-attribute'>
                         Last Name:
-                        <input className='input-text' type="text" name="lastName"/>
+                        <input id="lastName" name="lastName"className='input-text' type="text" 
+                        onChange={(e) => setLName(e.target.value)}/>
+                                                
                     </label>
                     </div>
                     <label className='contact-form-attribute'>
                         Your Email:
-                        <input className='input-text' type="text" name="email"/>
+                        <input id="email"name="email"className='input-text' type="text" 
+                        onChange={(e) => setEmail(e.target.value)}/>
                     </label>
                     <label className='contact-form-attribute'>
                         Your Message:
-                        <textarea  className='message-box' name="message"/>
+                        <textarea  id="message"name="message" className='message-box' 
+                                    onChange={(e) => setMessage(e.target.value)}/>
                     </label>
-                    <input className='contact-submit' type="submit" value="Submit"/>
+                    <button className='contact-submit' type="submit" >Submit</button>
                 </form>
            
             </div>
