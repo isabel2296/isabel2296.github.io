77765d1ac82f4e24ad1bc0fc972190655f69b8a4
Isabel Silva <70250018+isabel2296@users.noreply.github.com>
2023-07-26 21:55:07 -0700

    changed about

--- src/components/About.js
+++ src/components/About.js
@@ -25,10 +25,10 @@ function About() {
             <Button className="about-download-resume" href={resume_url} download="Silva-Isabel-Resume"> Download Resume </Button>
 
             <p className="about-summary">
-            I am skilled Software Engineer with a Master of Science in Software Engineering from California State University, Fullerton. During my academic journey, I achieved academic honors with a remarkable 3.97 GPA, reflecting my dedication and passion for the field.
-            <br/><br/> My technical expertise encompasses a wide range of programming languages, including Python, C/C++, SQL, JavaScript, and Java. Additionally, I am proficient in using various developer tools such as Git, VS Code, X Code, and more.
+            I am a skilled Software Engineer with a Master of Science in Software Engineering from California State University, Fullerton. Throughout my academic journey, I had the privilege of achieving academic honors with a remarkable 3.97 GPA, which reflects my unwavering dedication and immense passion for the field.            
+            <br/><br/> My technical expertise encompasses a wide range of programming languages, including Python, C/C++, SQL, JavaScript, and Java. Additionally, I am proficient in using various developer tools such as Git, VS Code, X Code, and more. Embracing new challenges and continuously seeking opportunities to grow and learn are qualities that define me both personally and professionally.
                 <br/><br/>
-                I have a strong passion for full stack web developing. 
+                I am a highly motivated individual with a deep passion for full-stack web development, backed by exceptional technical skills and a genuine love for computing. Besides my enthusiasm for technology, I also cherish spending quality time with my family, unwinding with video games, exploring new places through travel, and fostering meaningful connections with others. I am known for being an adaptable learner, a reliable team player, and always eager to embrace new opportunities for growth and learning.
                 <br/><br/> 
                 
             </p>                
