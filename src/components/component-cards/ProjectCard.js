dbf758db19ab23233dfb999498dc5fc929b9545f
Isabel Silva <70250018+isabel2296@users.noreply.github.com>
2023-09-20 11:34:17 -0700

    Updated project page
    enhanced the project page to utilize project cards. project cards take information from the data file containing project information.

--- src/components/component-cards/ProjectCard.js
+++ src/components/component-cards/ProjectCard.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { Divider, Input, QRCode, Space } from 'antd';
 import '../../styles/projectCard.css';
-import { GithubOutlined} from '@ant-design/icons'
-import { PassThrough } from 'nodemailer/lib/xoauth2';
-
-
+import { GithubOutlined} from '@ant-design/icons';
+import linkIconLight from '../../images/icons8-link-48-light.png';
+import linkIconDark from '../../images/icons8-link-48-dark.png'
 // key={x.id}
 // id={x.id}
 // category={x.category}
@@ -18,20 +17,28 @@ import { PassThrough } from 'nodemailer/lib/xoauth2';
 
 function ProjectCard(props){
     // state variables
-
+    const languageIcons = props.languages.map(x=>{
+        return <img className="carousel-content-lang-icons" src={`../images/${x}`} />
+    })
+    const currentTheme= localStorage.getItem('theme');
     return(
         <section className='project-card-wrapper'>
             <div className='project-card-header'>
-                <h3 className='project-title'>{props.title} ({props.year})</h3>
-
-                {/* {props.projectUrl? (<p>yes</p>):({})} */}
+                <div className='project-card-header-title-w-link'>
+                    <h4 className='project-title'>{props.title}  </h4>
+                    <div>{props.projectUrl? (<a  href={props.projectUrl} target="_blank" rel="noopener noreferrer">
+                    <img className="project-link-icon" src={currentTheme=="theme-light"?linkIconLight : linkIconDark} alt="Link Icon" /> </a>):(<span/> )}</div>
+                </div>
+                <p>({props.year})  </p>
 
             </div>
             <div className='project-card-body'>
+            <a className="project-card-languages">{languageIcons}</a>
+
                 <p>{props.description}</p>
             </div>
             <div className='project-card-links'>
-            <Divider style={{ borderColor: 'var(--light-text)' }}></Divider>
+            {/* <Divider style={{ borderColor: 'var(--light-text)' }}></Divider> */}
 
             <a href={props.githubUrl} target="_blank" rel="noopener noreferrer">
                     <GithubOutlined
